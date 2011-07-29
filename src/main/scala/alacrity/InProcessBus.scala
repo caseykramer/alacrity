4adace63bbe94b6c636b7089db07442ef498f4fb
Casey.Kramer <casey.kramer@gmail.com>
2011-07-28 21:37:17 -0400

    Update handler data structure to use WeakReference[Handler[_]].  I'm also pruning expired references when subscribers are added.  I could probably do it elsewhere as well, just to keep things clean

--- src/main/scala/alacrity/InProcessBus.scala
+++ src/main/scala/alacrity/InProcessBus.scala
@@ -1,25 +1,29 @@
 package alacrity
 
+import scala.ref.WeakReference
+
 class InProcessBus extends IBus {
 
-  var myHandler = Map[Manifest[_],List[Handler[_]]]()
+  var myHandler = Map[Manifest[_],List[WeakReference[Handler[_]]]]()
 
   def publish[T <: Message](message:T)(implicit m:Manifest[T]) {
     myHandler.filter(k => k._1 >:> m)
-             .foreach(k => k._2.foreach(h => h.asInstanceOf[Handler[T]].Handle(message)))
+             .foreach(k => k._2.filter(r => r.get.isDefined).foreach(h => h.get.get.asInstanceOf[Handler[T]].Handle(message)))
   }
 
   def subscribe[T <: Message](handler:Handler[T])(implicit m:Manifest[T]) {
+    val handlerRef = new WeakReference(handler).asInstanceOf[WeakReference[Handler[_]]]
     myHandler.get(m) match {
-      case Some(h) => myHandler = myHandler + (m -> h.+:(handler))
-      case None => myHandler = myHandler + (m -> List(handler))
+      case Some(h) => myHandler = myHandler + (m -> h.filter(r => r.get.isDefined).+:(handlerRef))
+      case None => myHandler = myHandler + (m -> List(handlerRef))
     }
   }
 
   def subscribe[T <: Message](handler:T => Unit)(implicit m:Manifest[T]) {
+    val handlerRef = new WeakReference(Handler(handler)).asInstanceOf[WeakReference[Handler[_]]]
     myHandler.get(m) match {
-      case Some(h) =>  myHandler = myHandler + (m -> h.+:(Handler(handler)))
-      case None => myHandler = myHandler + (m -> List(Handler(handler)))
+      case Some(h) =>  myHandler = myHandler + (m -> h.filter(r => r.get.isDefined).+:(handlerRef))
+      case None => myHandler = myHandler + (m -> List(handlerRef))
     }
   }
 }
